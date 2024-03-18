219e93bd6b0902690bad845fb1480857c888e9aa
nthapa000 <nishantthapa0000@gmail.com>
2024-03-18 23:06:17 +0530

    Step-5(Add cors, body parser and jsonwebtoken)

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    JWT_SECRET:"password"
+    // JWT SECRET
+}
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,13 @@
 const express = require("express");
-const app = express();
-
+const cors = require('cors')
 const rootRouter = require("./routes/index");
+// const bodyParser = require('body-parser')
+
+const app = express();
+app.use(cors())
+// no need to use this anymore
+// app.use(bodyParser.json()); //utilizes the body-parser package
+app.use(express.json())
+
 app.use("/api/v1",rootRouter)
+app.listen(3000);
