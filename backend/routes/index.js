f3961404184da60ddc34f8d98fcb343b645108a1
nthapa000 <nishantthapa0000@gmail.com>
2024-03-20 04:34:33 +0530

    Step 12: Setting up Account router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const userRouter = require('./user');
+const accountRouter = require('./account')
 
 const router = express.Router();
-router.use("/user",userRouter)
+
+router.use("/user",userRouter);
+// we are basically telling that if a request come here go here and if a request come there go there
+router.use('/account',accountRouter);
 
 module.exports = router;
