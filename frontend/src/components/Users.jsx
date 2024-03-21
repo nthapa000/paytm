f18f127437dc0d76d1629c00e4b3d168a6a09be5
nthapa000 <nishantthapa0000@gmail.com>
2024-03-21 21:26:09 +0530

    Final Commit

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -1,17 +1,20 @@
 import { useEffect, useState } from "react"
 import { Button } from "./Button"
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export const Users = () => {
     // Replace with backend call
     const [users, setUsers] = useState([]);
     const [filter,setFilter] = useState("");
+
+    // debouncing
     useEffect(()=>{
-        axios.get("http://localhost:3000/api/v1/user/bulk?filter"+filter)
+        axios.get("http://localhost:3000/api/v1/user/bulk?filter="+ filter)
         .then(response => {
             setUsers(response.data.user)
         })
-    },[])
+    },[filter])
     // currently it runs only once
     // in real world we have millions users 
     //  we do pagination and backend shows kind of 10 users
@@ -34,6 +37,7 @@ export const Users = () => {
 }
 
 function User({user}) {
+    const navigate = useNavigate();
     return <div className="flex justify-between">
         <div className="flex">
             <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
@@ -49,7 +53,10 @@ function User({user}) {
         </div>
 
         <div className="flex flex-col justify-center h-ful">
-            <Button label={"Send Money"} />
+            <Button onClick={(e)=>{
+                navigate("/send?id="+user._id+"&name="+user.firstName)
+                // We can also use LINK 
+            }} label={"Send Money"} />
         </div>
     </div>
 }
